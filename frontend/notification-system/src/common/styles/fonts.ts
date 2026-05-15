ddd9580fdd8880bb341ab16afd5bfab7630339d9
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-05-15 17:28:30 +0530

    Configuring custom fonts for the application

---
+++ frontend/notification-system/src/common/styles/fonts.ts
@@ -0,0 +1,23 @@
+import { Fira_Code, Plus_Jakarta_Sans, Sora } from 'next/font/google';
+
+
+// defining custom fonts to be used for the application
+export const jakartaSans = Plus_Jakarta_Sans({
+  variable: '--jakartaSans-font',
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
+});
+
+export const firaCode = Fira_Code({
+  variable: '--font-fira-code',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const soraSans = Sora({
+  variable: '--soraSans-font',
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
+});
