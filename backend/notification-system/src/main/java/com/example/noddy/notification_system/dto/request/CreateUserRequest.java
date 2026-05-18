a65c2154e181555548be20ccf919f4b01f549121
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-05-18 11:24:38 +0530

    Create request object for user data

---
+++ backend/notification-system/src/main/java/com/example/noddy/notification_system/dto/request/CreateUserRequest.java
@@ -0,0 +1,71 @@
+package com.example.noddy.notification_system.dto.request;
+
+import com.example.noddy.notification_system.constants.RoleConstants;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class CreateUserRequest {
+
+    @NotBlank(message = "username is required")
+    private String userName;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+    private String password;
+
+    @NotBlank(message = "mobile number is needed")
+    private String mobileNumber;
+
+    @NotNull(message = "At-least one Role is required")
+    private RoleConstants role;
+
+    public CreateUserRequest(){}
+
+    public CreateUserRequest(String userName, String name, String password, String mobileNumber, RoleConstants role) {
+        this.userName = userName;
+        this.name = name;
+        this.password = password;
+        this.mobileNumber = mobileNumber;
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public RoleConstants getRole() {
+        return role;
+    }
+
+    public void setRole(RoleConstants role) {
+        this.role = role;
+    }
+}
