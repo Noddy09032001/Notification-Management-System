337bbc9106af898e82b943a8fea495731a4cbd4c
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-05-22 16:50:08 +0530

    Adding code for jwt security workflows and custom id generators

--- backend/notification-system/src/main/java/com/example/noddy/notification_system/utils/NotificationIdGenerator.java
+++ backend/notification-system/src/main/java/com/example/noddy/notification_system/utils/NotificationIdGenerator.java
@@ -1,7 +1,10 @@
 package com.example.noddy.notification_system.utils;
 
+import org.springframework.stereotype.Component;
+
 import java.util.UUID;
 
+@Component
 public class NotificationIdGenerator {
     public NotificationIdGenerator(){}
 
