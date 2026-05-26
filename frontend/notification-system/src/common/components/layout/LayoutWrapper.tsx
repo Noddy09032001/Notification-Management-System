25fc8a821724adf6f3469002c871900e8e5e9f8e
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-05-26 11:35:04 +0530

    Added new code for the home layout to display login and signup pages

---
+++ frontend/notification-system/src/common/components/layout/LayoutWrapper.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import Layout from "."
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const LayoutWrapper = ({children,}: Props) => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+    setLoading(false);
+  }, []);
+
+  // Prevent hydration mismatch
+  if (loading) return null;
+
+  // If logged in -> show dashboard layout
+  if (isAuthenticated) {
+    return (
+      <Layout>
+        {children}
+      </Layout>
+    );
+  }
+
+  // Else -> show login page only
+  return <>{children}</>;
+};
+
+export default LayoutWrapper;
