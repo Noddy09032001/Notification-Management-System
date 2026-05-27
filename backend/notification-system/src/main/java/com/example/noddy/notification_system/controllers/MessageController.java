ab16c5b6d759bfb09b3e19baf1a7b7cea45b771a
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-05-27 14:26:10 +0530

    Added controller layer methods for login and signup

--- backend/notification-system/src/main/java/com/example/noddy/notification_system/controllers/MessageController.java
+++ backend/notification-system/src/main/java/com/example/noddy/notification_system/controllers/MessageController.java
@@ -1,9 +1,13 @@
 package com.example.noddy.notification_system.controllers;
 
 import com.example.noddy.notification_system.dto.request.CreateNotificationRequest;
+import com.example.noddy.notification_system.pojo.NotificationData;
+import com.example.noddy.notification_system.service.NotificationService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/notifications")
 public class MessageController {
 
+    private NotificationService notificationService;
+
+    @Autowired
+    public MessageController(NotificationService notificationService){
+        this.notificationService = notificationService;
+    }
+
     @PostMapping
-    public ResponseEntity<?> createMessageData(@Valid @RequestBody CreateNotificationRequest request){
-        return ResponseEntity
+    @PreAuthorize("hasAuthority('SEND_NOTIFICATION')")
+    public ResponseEntity<?> createMessageData(@Valid @RequestBody CreateNotificationRequest request) throws Exception {
+        NotificationData data =  notificationService.createNotificationRequestData(request);
+        return data != null ? ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body("Notification created successfully");
+                .body("Notification created successfully") : ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Notification could not be created");
     }
 }
---
+++ backend/notification-system/src/main/java/com/example/noddy/notification_system/controllers/RbacSettingsController.java
@@ -0,0 +1,39 @@
+package com.example.noddy.notification_system.controllers;
+
+import com.example.noddy.notification_system.dto.request.AuthUserRequest;
+import com.example.noddy.notification_system.dto.request.CreateNotificationRequest;
+import com.example.noddy.notification_system.dto.request.CreateUserRequest;
+import com.example.noddy.notification_system.dto.response.AuthResponse;
+import com.example.noddy.notification_system.service.rbacService.RbacService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class RbacSettingsController {
+
+    private RbacService rbacService;
+
+    @Autowired
+    public RbacSettingsController(RbacService rbacService){
+        this.rbacService = rbacService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Valid @RequestBody AuthUserRequest request) throws Exception {
+        AuthResponse response = rbacService.login(request);    // calling the login method for the same
+        return ResponseEntity.ok(response);   // storing the response from the same
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> signUpUser(@Valid @RequestBody CreateUserRequest request) throws Exception {
+        rbacService.createUser(request);    // calling the login method for the same
+        return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
+    }
+}
