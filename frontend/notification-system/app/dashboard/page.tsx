726064654665cefa18cc3317e5d6b0610ec87548
Noddy09032001 <niranjan.dhokarikar2001@gmail.com>
2026-05-29 12:35:48 +0530

    Modifying page component for dashboard module

--- frontend/notification-system/app/dashboard/page.tsx
+++ frontend/notification-system/app/dashboard/page.tsx
@@ -1,2 +1,10 @@
+import Dashboard from "@/src/modules/dashboard"
+
 const PAGE_TITLE = ""
 const PAGE_DESCRIPTION = ""
+
+export default function Page(){
+    return(
+        <Dashboard></Dashboard>
+    )
+}
